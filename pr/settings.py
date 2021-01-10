8639fb2beae26c02b3e4dba117a4e92c98fe5fc5
bea <developer2122@outlook.com>
2021-01-09 23:06:06 -0800

    fixed

--- pr/settings.py
+++ pr/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '05%dibzv(zw)i*lid0$2+uh8k-@llvq$(t1a-3*&4j(3mgr+qm'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 #'manageproject.herokuapp.com/', '127.0.0.1'
-ALLOWED_HOSTS = ['manageproject.herokuapp.com']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -77,12 +77,7 @@ WSGI_APPLICATION = 'pr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
